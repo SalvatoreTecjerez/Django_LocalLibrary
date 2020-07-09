6bdc6a2a66eb9f1fef7dbd5a8bd68f23823ba286
SalvatoreTecjerez <salvador.acevedo@tecjerez.edu.mx>
2020-07-08 20:05:00 -0500

    Update ALLOWED_HOSTS with site and development server URL

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -31,7 +31,7 @@ DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 
 
-ALLOWED_HOSTS = ['afternoon-atoll-72242.herokuapp.com/', '127.0.0.1']
+ALLOWED_HOSTS = ['afternoon-atoll-72242.herokuapp.com/', '127.0.0.1', 'crecimientomicrobiano-72242.herokuapp.com/']
 
 
 # Application definition
